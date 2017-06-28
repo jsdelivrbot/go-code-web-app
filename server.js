794a5ed2d6f25cefad20403b540f2927007913b9
Trey Lowerison <tlowerison@berkeley.edu>
2017-06-28 15:39:21 -0700

    removed some dependencies and simplified server.js

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 var express = require('express');
 var path = require('path');
 var compression = require('compression');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var dotenv = require('dotenv');
-var mongoose = require('mongoose');
-
-// Load environment variables from .env file
-dotenv.load();
 
 var app = express();
 
-
-mongoose.connect(process.env.MONGODB);
-mongoose.connection.on('error', function() {
-  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
-
 app.set('port', process.env.PORT || 3000);
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function(req, res) {
@@ -37,9 +19,5 @@ if (app.get('env') === 'production') {
     res.sendStatus(err.status || 500);
   });
 }
-
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
 
 module.exports = app;
